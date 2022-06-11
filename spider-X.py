db32f7b345372cc56c68d96e356c35f11a73ccd1
7-MAX25 <107028520+7-MAX25@users.noreply.github.com>
2022-06-11 19:01:37 +0200

    Add files via upload

---
+++ spider-X.py
@@ -0,0 +1,60 @@
+import os, sys,time  
+def jalan (z):
+        for e in z +'\n':
+                sys.stdout.write(e)
+                sys.stdout.flush ()
+                time.sleep (00000.05)
+os.system ('clear')
+print ("\033[1;31m")
+jalan ('Welcome to my tool')
+time.sleep(2)
+os.system ("clear")
+print ("\033[1;32m") 
+jalan ("""
+ ██▀▀▀▀███▀▀▀▀███▀▀▀▀██
+█┌─┐┌─┐█┌─┐┌─┐█┌─┐┌─┐█
+█└─┘└─┘█└─┘└─┘█└─┘└─┘█
+██▌└┘▐███▌└┘▐███▌└┘▐██
+██┼┼┼┼███┼┼┼┼███┼┼┼┼██
+██▄▄▄▄███▄▄▄▄███▄▄▄▄█""")
+os.system ('figlet spider-X')
+#########################
+print ("\033[1;34m")
+jalan ("To communicate with me") 
+print ("--------------------------------------------------------------------------------------------------------------") 
+jalan ("Telegram: https://t.me/REDSCORPIOIN") 
+jalan ("YouTube: https://youtube.com/channel/UCaJQuSLpT8-7l5VfY-d7h9A")
+print ("---------------------------------------------------------------------------------------------------------------") 
+#########################
+print ("\033[1;33m")
+jalan ("(1] ALIENS.HaCK.Camera") 
+jalan ("[2] LEDEAR_HACKING")
+jalan ("[3] Ddos404")
+jalan ("[4] Fetch the victim's IP") 
+jalan ("[5] hacker Social media") 
+
+ 
+list()
+print(" ")
+Choose = input(" [«] Choose :")
+os.system ("clear") 
+
+
+if Choose =='1':
+        os.system("git clone https://github.com/hackermohamedPro/ALIENS.HaCK.Camera;cd ALIENS.HaCK.Camera; bash alienshack.sh")
+        os.system ("python spider X.py") 
+
+elif Choose =="2": 
+	os.system("git clone https://github.com/Ledear-Hacker/LEDEAR_HACKING;cd LEDEAR_HACKING;python2 ledearhacking.py") 
+	os.system("python spider X.py")
+	
+elif Choose =="3": 
+	os.system("git clone https://github.com/CyberMrlink/Ddos404;cd Ddos404;sh ddos.sh")
+	os.system("python spider X.py")
+
+elif Choose =="4": 
+	os.system("xdg-open https://grabify.link/") 
+elif Choose  =="5": 
+	os.system("xdg-open http://sp0m.com/en/register") 
+	
+        
